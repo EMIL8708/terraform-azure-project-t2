d1ab5a04932b87e3bf8866bb0c2fd5a2e6a2e9dd
Murat <mcmura@inbox.ru>
2024-01-23 19:51:57 -0800

    update

--- MySQL/scaleset.tf
+++ MySQL/scaleset.tf
@@ -1,8 +1,16 @@
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_linux_virtual_machine_scale_set" "example" {
@@ -40,6 +48,4 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
       subnet_id = azurerm_subnet.internal.id
     }
   }
-
- 
 }
