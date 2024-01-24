5697b78239e2774ae8c950b2b9ca0ba05c98da53
Murat <mcmura@inbox.ru>
2024-01-23 18:57:40 -0800

    delete resource

--- MySQL/scaleset.tf
+++ MySQL/scaleset.tf
@@ -1,12 +1,3 @@
-
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
-}
-
-
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
