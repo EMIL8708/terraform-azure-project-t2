0e5ace60ed57459e438d59b9520b1322c50e69f8
EMIL MOLDOISAEV <92470787+EMIL8708@users.noreply.github.com>
2024-01-23 22:23:17 -0600

    Update main.tf

--- VNET/main.tf
+++ VNET/main.tf
@@ -1,37 +1,38 @@
 # Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "wordpress" {
+  name     = "wordpress-resources"
+  location = var.location
+  tags     = var.tags
 }
 
 # Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_virtual_network" "wordpress" {
+  name                = "wordpress-network"
+  resource_group_name = azurerm_resource_group.wordpress.name
+  location            = azurerm_resource_group.wordpress.location
   address_space       = ["10.0.0.0/16"]
 }
 
 
 resource "azurerm_subnet" "subnet1" {
   name                 = "subnet1"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.wordpress.name
+  virtual_network_name = azurerm_virtual_network.wordpress.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 
 resource "azurerm_subnet" "subnet2" {
   name                 = "subnet2"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.wordpress.name
+  virtual_network_name = azurerm_virtual_network.wordpress.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 
 resource "azurerm_subnet" "subnet3" {
   name                 = "subnet3"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.wordpress.name
+  virtual_network_name = azurerm_virtual_network.wordpress.name
   address_prefixes     = ["10.0.3.0/24"]
 }
