0c2f28b93c19d05b1dc5615a73732001d982423b
EMIL MOLDOISAEV <92470787+EMIL8708@users.noreply.github.com>
2024-01-23 22:51:44 -0600

    Update variable.tf

--- VNET/variable.tf
+++ VNET/variable.tf
@@ -1,6 +1,6 @@
 variable "location" {
   description = "The location where resources will be created"
-  default     = "us-east-1"
+  default     = "East US"
 }
 variable "tags" {
   description = "A map of the tags to use for the resources that are deployed"
