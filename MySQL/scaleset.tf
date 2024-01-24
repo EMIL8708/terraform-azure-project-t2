bb3c10ebe52fbf6eed80134b5101101adbe1ddf0
Murat <mcmura@inbox.ru>
2024-01-23 19:13:22 -0800

    subnet

--- MySQL/scaleset.tf
+++ MySQL/scaleset.tf
@@ -1,5 +1,5 @@
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+resource "azurerm_subnet" "subnet2" {
+  name                 = "subnet2"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
@@ -35,7 +35,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
     primary = true
 
     ip_configuration {
-      name      = "internal"
+      name      = "subnet2"
       primary   = true
       subnet_id = azurerm_subnet.internal.id
     }
