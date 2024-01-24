a69b23d214a8d36ba538c2da294bb30c1d613e0b
Murat <mcmura@inbox.ru>
2024-01-23 19:20:25 -0800

    update vnet files

--- MySQL/vnet.tf
+++ MySQL/vnet.tf
@@ -1,40 +1,34 @@
-# Create a resource group
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "West Europe"
 }
 
-# Create a virtual network within the resource group
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.example.name
+
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  subnet {
+    name           = "subnet3"
+    address_prefix = "10.0.3.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
 }
-
-
-resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet1"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-
-resource "azurerm_subnet" "subnet4" {
-  name                 = "subnet4"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-
-resource "azurerm_subnet" "subnet3" {
-  name                 = "subnet3"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.3.0/24"]
-}
-
-
-
