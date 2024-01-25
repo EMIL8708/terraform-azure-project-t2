8bc320fca8f08b48777f4c2299328d198443f23f
Murat <mcmura@inbox.ru>
2024-01-25 14:24:59 -0800

    rt added

---
+++ MySQL/RT.tf
@@ -0,0 +1,11 @@
+resource "azurerm_route_table" "example" {
+  name                          = "example-route-table"
+  location                      = azurerm_resource_group.example.location
+  resource_group_name           = azurerm_resource_group.example.name
+  disable_bgp_route_propagation = false
+
+  route {
+    name           = "route1"
+    address_prefix = "10.0.1.0/24"
+    next_hop_type  = "VnetLocal"
+  }
