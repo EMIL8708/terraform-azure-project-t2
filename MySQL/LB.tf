f0eeaaac440159f14c9cf2a996a7a668389831dc
Murat <mcmura@inbox.ru>
2024-01-25 13:54:44 -0800

    update

--- MySQL/LB.tf
+++ MySQL/LB.tf
@@ -17,9 +17,8 @@ resource "azurerm_lb_backend_address_pool" "bpepool" {
 }
 
 resource "azurerm_lb_probe" "example" {
-  resource_group_name = azurerm_resource_group.example.name
   loadbalancer_id     = azurerm_lb.example.id
-  name                = "example-resource"
+  name                = "example-probe"
   port                = var.application_port
 }
 
