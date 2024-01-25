e1601b7e292766a54e582ea3853a5368ccb34f84
EMIL MOLDOISAEV <92470787+EMIL8708@users.noreply.github.com>
2024-01-25 12:34:44 -0600

    changed backend

--- MySQL/backend.tf
+++ MySQL/backend.tf
@@ -1,10 +1,4 @@
 terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.91.0"
-    }
-  }
   backend "azurerm" {
         resource_group_name  = "cloud-shell-storage-eastus" #resource resource_group_name created manually 
         storage_account_name = "cs2100320034430688d"     # created manually
