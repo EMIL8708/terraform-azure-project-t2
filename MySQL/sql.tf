2f23602c6f15b79580e283d6154595ef6ab248b5
EMIL MOLDOISAEV <92470787+EMIL8708@users.noreply.github.com>
2024-01-25 15:09:49 -0600

    changed sql

--- MySQL/sql.tf
+++ MySQL/sql.tf
@@ -42,8 +42,8 @@ resource "azurerm_mysql_firewall_rule" "example" {
   name                = "example-mysql-firewall-rule"
   resource_group_name = azurerm_resource_group.example.name
   server_name         = azurerm_mysql_server.example.name
-  start_ip_address    = azurerm_public_ip.example.end_ip_address
-  end_ip_address      = azurerm_public_ip.example.end_ip_address
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "255.255.255.255"
 }
 
 data "azurerm_mysql_server" "example" {
