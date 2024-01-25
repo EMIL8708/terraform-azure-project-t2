e47c23500f9a7dd7b5a3f8223f92005095d74747
EMIL MOLDOISAEV <92470787+EMIL8708@users.noreply.github.com>
2024-01-25 00:43:08 -0600

    changed vnet

--- MySQL/vnet.tf
+++ MySQL/vnet.tf
@@ -1,10 +1,3 @@
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = var.location
-  tags     = var.tags
-}
-
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
