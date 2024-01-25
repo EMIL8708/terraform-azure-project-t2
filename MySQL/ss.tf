31856342f7a259565c2eec62f171ca211b1c1e7e
Murat <mcmura@inbox.ru>
2024-01-25 14:00:59 -0800

    updated

--- MySQL/ss.tf
+++ MySQL/ss.tf
@@ -15,7 +15,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("${var.ssh_public_key_path}")
+    public_key = "~/.ssh_public_key_path"
   }
 
   source_image_reference {
