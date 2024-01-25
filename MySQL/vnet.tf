acb22c2986f53956e6ad5323f4477a75b3de62b5
Murat <mcmura@inbox.ru>
2024-01-24 20:02:44 -0800

    update region

--- MySQL/vnet.tf
+++ MySQL/vnet.tf
@@ -1,9 +1,3 @@
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "East US"
-}
-
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
