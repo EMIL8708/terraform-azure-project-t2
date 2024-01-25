d041d3465e1ec358cb61f12ab9f498823b079ea0
EMIL MOLDOISAEV <92470787+EMIL8708@users.noreply.github.com>
2024-01-25 00:41:12 -0600

    added resource group

--- MySQL/vnet.tf
+++ MySQL/vnet.tf
@@ -1,3 +1,10 @@
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = var.location
+  tags     = var.tags
+}
+
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
@@ -6,7 +13,6 @@ resource "azurerm_virtual_network" "example" {
   address_space       = ["10.0.0.0/16"]
 }
 
-
 resource "azurerm_subnet" "subnet1" {
   name                 = "subnet1"
   resource_group_name  = azurerm_resource_group.example.name
@@ -14,14 +20,12 @@ resource "azurerm_subnet" "subnet1" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-
 resource "azurerm_subnet" "subnet2" {
   name                 = "subnet2"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
 }
-
 
 resource "azurerm_subnet" "subnet3" {
   name                 = "subnet3"
@@ -31,8 +35,6 @@ resource "azurerm_subnet" "subnet3" {
 }
 
 data "azurerm_public_ip" "example" {
-  name                = "name_of_public_ip"
-  resource_group_name = "name_of_resource_group"
+  name                = "example"
+  resource_group_name = "example"
 }
-
-
