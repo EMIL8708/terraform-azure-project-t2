fe8b534963d6056c85ef28c44c696935e9b83c60
Murat <mcmura@inbox.ru>
2024-01-25 13:31:55 -0800

    added route53

--- MySQL/dns.tf
+++ MySQL/dns.tf
@@ -1,12 +1,13 @@
-resource "azurerm_dns_zone" "example" {
-  name                = "moldoisaevemgmail.onmicrosoft.com"
-  resource_group_name = azurerm_resource_group.example.name
+provider "aws" {
+    region = "us-east-1"
+    access_key= "AKIAW7TLTGPNZFMXNCJE"
+    secret_key= "NfDOfttgvVY/m+Z/bJd6EQeoL5WtCZs6Aa2ICODk"
 }
 
-resource "azurerm_dns_a_record" "example" {
-  name                = "test"
-  zone_name           = azurerm_dns_zone.example.name
-  resource_group_name = azurerm_resource_group.example.name
-  ttl                 = 300
-  records             = ["10.0.180.17"]
+resource "aws_route53_record" "wordpress" {
+  zone_id = "Z056284239QHHUXN5IEWO"
+  name    = "wordpress.muratgroup.link"
+  type    = "A"
+  ttl     = 300
+  records = [azurerm_public_ip.example.ip_address]
 }
