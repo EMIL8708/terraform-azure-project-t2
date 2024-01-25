905e9a00790ba796111b7f928e7ece9413d6f869
Murat <mcmura@inbox.ru>
2024-01-24 20:22:21 -0800

    update route53

--- MySQL/sec.group.tf
+++ MySQL/sec.group.tf
@@ -31,6 +31,6 @@ resource "azurerm_network_security_group" "example" {
 
 #Assigning a security group to all existing subnets
 resource "azurerm_subnet_network_security_group_association" "nsg-assoc" {
-  subnet_id                 = azurerm_subnet.wordpress.id
+  subnet_id                 = azurerm_subnet.example.id
   network_security_group_id = azurerm_network_security_group.example.id
 }
