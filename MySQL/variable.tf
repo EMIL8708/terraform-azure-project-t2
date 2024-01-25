176f185d5fd0affcda4f1109855655031876dbde
Murat <mcmura@inbox.ru>
2024-01-24 19:16:49 -0800

    update

--- MySQL/variable.tf
+++ MySQL/variable.tf
@@ -1,13 +1,15 @@
 variable "location" {
   description = "The location where resources will be created"
-  default     = "East US"
+  type        = string
+  default     = ""
 }
 
 variable "tags" {
   description = "A map of the tags to use for the resources that are deployed"
   type        = map(string)
+
   default = {
-    environment = "WordPress"
+    environment = "DevOps"
   }
 }
 
@@ -23,7 +25,15 @@ variable "admin_username" {
 
 variable "admin_password" {
   description = "Default password for admin account"
-  default     = "w0rdpr3sS"
+  default     = "26F4QXHVYbBjC$WH2HAc"
+}
+
+variable "dbname"{
+  default = "db-wordpress-team2-aug22"
+}
+
+variable "db_server_name" {
+ default = "team2-db-server-wordpress"
 }
 
 variable "database_admin_login" {
@@ -31,5 +41,22 @@ variable "database_admin_login" {
 }
 
 variable "database_admin_password" {
-  default = "w0rdpr3sS"
+  default = "26F4QXHVYbBjC$WH2HAc"
+}
+
+variable "cidr_block" {
+  description = "Provide CIDR Block"
+  type        = list
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_address" {
+  description = "Provide subnet address space"
+  type        = list
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "sec_group" {
+  description = "Enter Security Group name"
+  default     = "wordpress_sec_group"
 }
