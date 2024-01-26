53f83658f418c0aefd00768349dfff0b9cbf7956
Murat <mcmura@inbox.ru>
2024-01-25 16:35:15 -0800

    update

--- MySQL/ss.tf
+++ MySQL/ss.tf
@@ -1,5 +1,3 @@
-
-
 resource "azurerm_linux_virtual_machine_scale_set" "example" {
   name                = "example-vmss"
   resource_group_name = azurerm_resource_group.example.name
@@ -7,7 +5,6 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
   sku                 = "Standard_F2"
   instances           = 1
   admin_username      = "adminuser"
-
 
   admin_ssh_key {
     username   = "adminuser"
@@ -28,20 +25,18 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
 
   network_interface {
     name    = "example"
-    primary = true 
-  }
+    primary = true
 
     ip_configuration {
       name      = "subnet3"
       primary   = true
       subnet_id = azurerm_subnet.subnet3.id
-      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id] 
+    }
+  }
 
   virtual_machine_profile {
     os_profile {
-      custom_data = file("custom_data.tpl") 
-
+      custom_data = file("custom_data.tpl")
     }
   }
-}
 }
