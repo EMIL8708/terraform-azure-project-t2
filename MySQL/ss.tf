ab25b1452ee866c94c4f1cfb2b20d6feda0e14c2
Murat <mcmura@inbox.ru>
2024-01-25 16:44:12 -0800

    update

--- MySQL/ss.tf
+++ MySQL/ss.tf
@@ -23,6 +23,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
     caching              = "ReadWrite"
   }
 
+virtual_machine_profile {
   os_profile {
     custom_data = file("custom_data.tpl")
     }
@@ -38,7 +39,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
     }
   }
 
-  
+} 
     
   }
 
