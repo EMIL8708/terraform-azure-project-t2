a73d3528c5d8c918f82ee8dc603836e9a8e28d10
Murat <mcmura@inbox.ru>
2024-01-27 11:18:31 -0800

    updated

--- MySQL/IGW.tf
+++ MySQL/IGW.tf
@@ -22,6 +22,6 @@ resource "azurerm_virtual_network_gateway" "example" {
     name                          = "vnetGatewayConfig"
     public_ip_address_id          = azurerm_public_ip.igw.id
     private_ip_address_allocation = "Dynamic"
-    subnet_id                     = azurerm_subnet2.example.id
+    subnet_id                     = azurerm_subnet.subnet2.id
   }
 }
