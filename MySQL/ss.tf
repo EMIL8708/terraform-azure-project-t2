4d8fd7a316af76d75db15ec214436c466069507a
Murat <mcmura@inbox.ru>
2024-01-26 22:54:16 -0800

    updated

--- MySQL/ss.tf
+++ MySQL/ss.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "example" {
-  name                 = "acctsub"
+  name                 = "subnet2"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
