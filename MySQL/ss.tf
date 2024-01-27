7d8f578dca3457ef1653d3307cce14211abbe5b1
Murat <mcmura@inbox.ru>
2024-01-27 15:46:55 -0800

    port

--- MySQL/ss.tf
+++ MySQL/ss.tf
@@ -50,6 +50,7 @@ resource "azurerm_lb_backend_address_pool" "bpepool" {
 resource "azurerm_lb_probe" "example" {
   loadbalancer_id = azurerm_lb.example.id
   name            = "http-probe"
+  port            = 80
   request_path    = "/health"
 }
 
