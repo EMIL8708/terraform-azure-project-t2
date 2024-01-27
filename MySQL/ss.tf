55629abe6d1090188f27b5c4200dce9738139c5c
Murat <mcmura@inbox.ru>
2024-01-27 15:31:36 -0800

    update

--- MySQL/ss.tf
+++ MySQL/ss.tf
@@ -28,21 +28,24 @@ resource "azurerm_lb" "example" {
   }
 }
 
+resource "azurerm_lb_rule" "example" {
+  loadbalancer_id                = azurerm_lb.example.id
+  name                           = "http"
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "Public-IP"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpepool.id]
+  probe_id                       =  azurerm_lb_probe.example.id  
+}
+
+
 resource "azurerm_lb_backend_address_pool" "bpepool" {
   loadbalancer_id     = azurerm_lb.example.id
   name                = "BackEndAddressPool"
 }
 
-resource "azurerm_lb_nat_pool" "lbnatpool" {
-  resource_group_name            = azurerm_resource_group.example.name
-  name                           = "ssh"
-  loadbalancer_id                = azurerm_lb.example.id
-  protocol                       = "Tcp"
-  frontend_port_start            = 50000
-  frontend_port_end              = 50119
-  backend_port                   = 22
-  frontend_ip_configuration_name = "PublicIPAddress"
-}
+
 
 resource "azurerm_lb_probe" "example" {
   loadbalancer_id = azurerm_lb.example.id
@@ -59,7 +62,9 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
   sku                 = "Standard_D2S_v3"
   instances           = 2
   admin_username      = "adminuser"
+  health_probe_id     = azurerm_lb_probe.example.id
   custom_data         = filebase64("customdata.tpl")
+  
 
   admin_ssh_key {
     username   = "adminuser"
