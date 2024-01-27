1b8750061c20a7ce053ecad6fc31f8e26cec5437
Murat <mcmura@inbox.ru>
2024-01-26 23:09:58 -0800

    updated

--- MySQL/ss.tf
+++ MySQL/ss.tf
@@ -29,8 +29,9 @@ resource "azurerm_lb" "example" {
 }
 
 resource "azurerm_lb_backend_address_pool" "bpepool" {
-  loadbalancer_id = azurerm_lb.example.id
-  name            = "BackEndAddressPool"
+  loadbalancer_id     = azurerm_lb.example.id
+  resource_group_name = azurerm_resource_group.example.name
+  name                = "BackEndAddressPool"
 }
 
 resource "azurerm_lb_nat_pool" "lbnatpool" {
@@ -47,13 +48,13 @@ resource "azurerm_lb_nat_pool" "lbnatpool" {
 resource "azurerm_lb_probe" "example" {
   loadbalancer_id = azurerm_lb.example.id
   name            = "http-probe"
-  protocol        = "Http"
+  protocol        = "http"
   request_path    = "/health"
-  port            = 8080
+  port            = 80
 }
 
 resource "azurerm_virtual_machine_scale_set" "example" {
-  name                = "mytestscaleset-1"
+  name                = "example-vmss"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
