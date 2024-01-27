9a645ee3faec8b3818db948baf5e921c8b40c9f1
Murat <mcmura@inbox.ru>
2024-01-27 11:15:26 -0800

    updated

---
+++ MySQL/IGW.tf
@@ -0,0 +1,27 @@
+resource "azurerm_public_ip" "IGW" {
+  name                = "Public-IP-IGW"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  allocation_method = "Dynamic"
+}
+
+resource "azurerm_virtual_network_gateway" "example" {
+  name                = "IGW"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  type     = "Vpn"
+  vpn_type = "RouteBased"
+
+  active_active = false
+  enable_bgp    = false
+  sku           = "Basic"
+
+  ip_configuration {
+    name                          = "vnetGatewayConfig"
+    public_ip_address_id          = azurerm_public_ip.example.id
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet2.example.id
+  }
+}
