3dad79bc1305efcb7a432617c07ee9bee57fd645
Murat <mcmura@inbox.ru>
2024-01-27 13:18:00 -0800

    updated

--- MySQL/ss.tf
+++ MySQL/ss.tf
@@ -66,6 +66,7 @@ resource "azurerm_linux_virtual_machine_scale_set" "example" {
     username   = "adminuser"
     public_key = file("~/.ssh/id_rsa.pub")
   }
+}
   
     source_image_reference {
     publisher = "OpenLogic"
