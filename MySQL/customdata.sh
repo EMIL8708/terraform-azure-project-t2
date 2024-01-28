d11e50dabde2f2d832cbb7c2d1d1b6fb0252c0c3
Murat <mcmura@inbox.ru>
2024-01-27 16:09:57 -0800

    updated

--- MySQL/customdata.sh
+++ MySQL/customdata.sh
@@ -1,30 +1,15 @@
 #!/bin/bash
-sudo yum install httpd wget unzip epel-release mysql -y
-sudo yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-sudo yum -y install yum-utils
-sudo yum-config-manager --enable remi-php56
-sudo yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
-sudo wget https://wordpress.org/latest.tar.gz
-sudo tar -xf latest.tar.gz -C /var/www/html/
-sudo mv /var/www/html/wordpress/* /var/www/html/
-sudo cp /var/www/html/wp-config-sample.php  /var/www/html/wp-config.php 
-sudo sed 's/database_name_here/db-wordpress-team2-aug22/g' /var/www/html/wp-config.php -i
-sudo sed 's/username_here/wordpress@team2-db-server-wordpress/g' /var/www/html/wp-config.php -i
-sudo sed 's/password_here/26F4QXHVYbBjC$WH2HAc/g' /var/www/html/wp-config.php -i
-sudo sed 's/localhost/team2-db-server-wordpress.mysql.database.azure.com/g' /var/www/html/wp-config.php -i
-#DBNAME="db-wordpress-team2-aug22"
-sudo getenforce
-sudo sed 's/SELINUX=permissive/SELINUX=enforcing/g' /etc/sysconfig/selinux -i
-sudo setenforce 0
-sudo chown -R apache:apache /var/www/html/
-sudo systemctl start httpd
+set -xe
+sudo yum install httpd php php-mysql*  wget  -y
+sudo systemctl restart httpd
 sudo systemctl enable httpd
-
-# DB_SERVER_NAME="team2-db-server-wordpress"
-
-
-# export WORDPRESS_DB_HOST= "${DB_SERVER_NAME}.mysql.database.azure.com"
-# export WORDPRESS_DB_USER= "wordpress@${DB_SERVER_NAME}"
-# export WORDPRESS_DB_PASSWORD= "26F4QXHVYbBjC$WH2HAc"
-# export WORDPRESS_DB_NAME= "${DBNAME}"
-
+sudo wget  https://wordpress.org/wordpress-4.0.32.tar.gz
+sudo tar -xf wordpress-4.0.32.tar.gz -C /var/www/html/
+sudo mv /var/www/html/wordpress/* /var/www/html/
+sudo systemctl restart httpd
+sudo chown -R apache:apache /var/www/html/
+echo "export WORDPRESS_DB_HOST=wordpressdb.bluegeezer.com" | sudo tee /etc/profile.d/wordpress.sh
+echo "export WORDPRESS_DB_NAME=wordpress" | sudo tee -a /etc/profile.d/wordpress.sh
+echo "export WORDPRESS_DB_USER=foo" | sudo tee -a /etc/profile.d/wordpress.sh
+echo "export WORDPRESS_DB_PASSWORD=foobarbaz" | sudo tee -a /etc/profile.d/wordpress.sh
+sudo systemctl restart httpd
