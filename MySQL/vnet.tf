b14087d454a42c0b371bee2b603ffcc09568a285
Murat <mcmura@inbox.ru>
2024-01-28 22:49:59 -0800

    added network

--- MySQL/vnet.tf
+++ MySQL/vnet.tf
@@ -26,3 +26,15 @@ resource "azurerm_subnet" "subnet3" {
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.3.0/24"]
 }
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet3.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
