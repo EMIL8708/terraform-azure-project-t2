aeb9f42240dfa9688d8702436425bc453c65e317
Murat <mcmura@inbox.ru>
2024-01-28 21:03:11 -0800

    updated

--- MySQL/ss.tf
+++ MySQL/ss.tf
@@ -36,7 +36,7 @@ resource "azurerm_lb_rule" "example" {
   backend_port                   = 80
   frontend_ip_configuration_name = "Public-IP"
   backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpepool.id]
-  probe_id                       =  azurerm_lb_probe.example.id  
+  probe_id                       = [azurerm_lb_probe.example.id]  
 }
 
 
