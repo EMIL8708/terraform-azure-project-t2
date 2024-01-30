c9a28540938c8cfce91a236bdaf4f25ab4ce5d44
EMIL MOLDOISAEV <92470787+EMIL8708@users.noreply.github.com>
2024-01-29 22:06:09 -0600

    chanched wordpress

--- MySQL/wordpress.sh
+++ MySQL/wordpress.sh
@@ -1,30 +1,24 @@
-# bash wordpress.sh
- provisioner "remote-exec" {
-  inline = [
-    sudo apt update
-    sudo apt install apache2 \
-                    ghostscript \
-                    libapache2-mod-php \
-                    mysql-server \
-                    php \
-                    php-bcmath \
-                    php-curl \
-                    php-imagick \
-                    php-intl \
-                    php-json \
-                    php-mbstring \
-                    php-mysql \
-                    php-xml \
-                    php-zip
- 
-    # Create the installation directory and download the file from WordPress.org:
-    sudo mkdir -p /srv/www
-    sudo chown www-data: /srv/www
-    curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
-  # Install MySQL
-    sudo apt install mysql-server
-  # Install PHP
-    sudo apt install php libapache2-mod-php php-mysql
-   ]
-   
-  }
+sudo apt update
+sudo apt install apache2 \
+              ghostscript \
+              libapache2-mod-php \
+              mysql-server \
+              php \
+              php-bcmath \
+              php-curl \
+              php-imagick \
+              php-intl \
+              php-json \
+              php-mbstring \
+              php-mysql \
+              php-xml \
+              php-zip -y 
+
+# Create the installation directory and download the file from WordPress.org:
+sudo mkdir -p /srv/www
+sudo chown www-data: /srv/www
+curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+# Install MySQL
+sudo apt install mysql-server -y
+# Install PHP
+sudo apt install php libapache2-mod-php php-mysql -y
