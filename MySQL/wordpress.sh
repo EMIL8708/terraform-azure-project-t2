b52d221ceeb83ad6d3b3261630911eb2242697c4
Murat <mcmura@inbox.ru>
2024-01-29 18:23:21 -0800

    updated

--- MySQL/wordpress.sh
+++ MySQL/wordpress.sh
@@ -21,6 +21,10 @@
     sudo mkdir -p /srv/www
     sudo chown www-data: /srv/www
     curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+  # Install MySQL
+    sudo apt install mysql-server
+  # Install PHP
+    sudo apt install php libapache2-mod-php php-mysql
    ]
    
   }
