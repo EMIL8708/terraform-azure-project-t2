27eef7d7f2d0007e0e42700371fd12ed8ac64955
Murat <mcmura@inbox.ru>
2024-01-29 17:19:18 -0800

    updated

--- MySQL/wordpress.sh
+++ MySQL/wordpress.sh
@@ -1,23 +1,26 @@
-#!/bin/bash 
-# install PHP and Apache, use following command:
-sudo apt update
-sudo apt install apache2 \
-                 ghostscript \
-                 libapache2-mod-php \
-                 mysql-server \
-                 php \
-                 php-bcmath \
-                 php-curl \
-                 php-imagick \
-                 php-intl \
-                 php-json \
-                 php-mbstring \
-                 php-mysql \
-                 php-xml \
-                 php-zip
-
-# Create the installation directory and download the file from WordPress.org:
-sudo mkdir -p /srv/www
-sudo chown www-data: /srv/www
-curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
-	
+# bash wordpress.sh
+ provisioner "remote-exec" {
+  inline = [
+    sudo apt update
+    sudo apt install apache2 \
+                    ghostscript \
+                    libapache2-mod-php \
+                    mysql-server \
+                    php \
+                    php-bcmath \
+                    php-curl \
+                    php-imagick \
+                    php-intl \
+                    php-json \
+                    php-mbstring \
+                    php-mysql \
+                    php-xml \
+                    php-zip
+ 
+    # Create the installation directory and download the file from WordPress.org:
+    sudo mkdir -p /srv/www
+    sudo chown www-data: /srv/www
+    curl https://wordpress.org/latest.tar.gz | sudo -u www-data tar zx -C /srv/www
+   ]
+   
+  }
