dd4c031cf6a4062ff9ede769f52bc7e45d0e7390
EMIL MOLDOISAEV <92470787+EMIL8708@users.noreply.github.com>
2024-01-29 21:14:21 -0600

    changed backend

--- MySQL/backend.tf
+++ MySQL/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
         resource_group_name  = "cloud-shell-storage-eastus" #resource resource_group_name created manually 
-        storage_account_name = "cs21003200342861b09"     # created manually
+        storage_account_name = "cs2100320034430688d"     # created manually
         container_name       = "tfstate"
         key                  = "wordpress.tfstate"
     }
